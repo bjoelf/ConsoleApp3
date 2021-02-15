8983a612d256a15d1808215256fb19efa91a09d9
Björn Elfvin <bjorn@elfvin.se>
2021-02-15 11:56:16 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! \nI LoveToCode");
+            Console.WriteLine("I just modified my code and pushed it up to github!");
+
+        }
+    }
+}
